9afc976d016ffc1939799c74dbe7dcfa534814ce
jellyfishing2346 <108194702+jellyfishing2346@users.noreply.github.com>
2026-02-02 12:06:30 -0500

    Updated diagnostic review code - completed

--- Week-1/diagnostic-review.py
+++ Week-1/diagnostic-review.py
@@ -5,10 +5,16 @@ sorted(set(data)) # [2, 3, 7, 9]
 
 # Question 2
 
-counts = []
+counts = {}
 for x in data: 
-    counts[x] = counts.get(x, 0) + 1
+    counts[x] = counts.get(x, 0) + 1 #{3: 2, 7: 2, 2: 2, 9: 1}
+
 
 # Question 3
+nums = [1, 2, 3, 4, 5]
+result = []
+for i in range(len(nums) - 1):
+    result.append(nums[i+1]-nums[i]) # [1, 1, 1, 1]
+
 
 
--- Week-2/window.py
+++ Week-2/window.py
@@ -1,3 +1,4 @@
+import timeit
 
 nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 def window_sum(nums, k):
@@ -22,7 +23,9 @@ def window_optimized(nums, k):
 print(window_optimized(nums,3))
 # Time complexity: O(n)
 
-t1 = timeit(lambda:window_sum(nums), number=10000)
+t1 = timeit.timeit(lambda:window_sum(nums, 3), number=10000)
+t2 = timeit.timeit(lambda:window_sum(nums, 3), number=10000)
+print(f"t1: {t1}, t2: {t2}")
         
 
    
